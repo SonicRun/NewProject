2883efae3e6b05600153c1ddca0463a2d03e055c
Vladimir <44170856+SonicRun@users.noreply.github.com>
2018-11-30 12:25:24 +0400

    обновление от 30.11.2018

--- src/main/java/JDBCtest.java
+++ src/main/java/JDBCtest.java
@@ -5,23 +5,18 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import java.util.Scanner;
 public class JDBCtest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         Connection connection = null;
-        //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
-        String url = "jdbc:postgresql://127.0.0.1:5432/test";
-        //Имя пользователя БД
-        String name = "postgres";
-        //Пароль
-        String password = "123456";
-         final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/test";
-         final String USER = "user";
-         final String PASS = "123456";
-
+        final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
+        final String USER = "user";
+        final String PASS = "123456";
+        Scanner in = new Scanner(System.in);
         System.out.println("Testing connection to PostgreSQL JDBC");
-
+        PreparedStatement stmt = null;
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -47,6 +42,24 @@ public class JDBCtest {
             System.out.println("You successfully connected to database now");
         } else {
             System.out.println("Failed to make connection to database");
+        }
+        stmt = connection.prepareStatement(" INSERT INTO users (login, password) values (?, ?)");
+        System.out.print(" Input a login of your user: ");
+        String g  = in.next();
+        System.out.print("\n");
+        stmt.setString(1, g);
+        System.out.print(" Input a password of your user: ");
+        String r  = in.next();
+        System.out.print("\n");
+        stmt.setString(2, r);
+        if (stmt.execute()) {
+            System.out.print(" Add user complate ");
+        }
+
+        stmt = connection.prepareStatement("SELECT * FROM users ");
+        ResultSet set = stmt.executeQuery();
+        while (set.next()) {
+            System.out.println(set.getString(1) + " " + set.getString(2) + " " + set.getInt(3));
         }
 
     }
